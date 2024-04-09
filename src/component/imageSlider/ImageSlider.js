b8be559f62bfe6142f0fd412710514559dba4237
sadeq-obeydi <sadeqby7@gmail.com>
2024-04-09 22:43:30 +0330

    condition with hook useStateClass

--- src/component/imageSlider/ImageSlider.js
+++ src/component/imageSlider/ImageSlider.js
@@ -7,20 +7,14 @@ import useStatusClass from "../../hooks/useStatusClass"
 function ImageSlider({slides , currentIndex , active , statusSlider , length}){
 
 
-    // let [prevAnimate , nextAnimate] = useStatusClass({slides , currentIndex , active , statusSlider , length})
+    let [prevAnimate , nextAnimate] = useStatusClass({slides , currentIndex , active , statusSlider , length})
 
     return(
 
         <div 
             key={active}
-            className={`image-slider 
-                ${ statusSlider === 'next' && currentIndex === active && 'next-next'}
-                ${statusSlider === 'next' && currentIndex > 0 && currentIndex - active === 1 && 'next-previous'}
+            className={`image-slider ${nextAnimate} ${prevAnimate}
                 
-                ${ statusSlider === 'previous' && currentIndex === active && 'previous-next'}
-                ${statusSlider === 'previous' && currentIndex > 0 && currentIndex - active === 1 && 'previous-previous'}
-
-               
             ` } 
             style={{backgroundImage:`url(${slides.url})`}}>
 
--- src/hooks/useStatusClass.js
+++ src/hooks/useStatusClass.js
@@ -1,5 +1,23 @@
-// function useStatusClass (){
+function useStatusClass ({slides , currentIndex , active , statusSlider , length}){
     
-// }
+    let prevAnimate = 'prev' 
+    let nextAnimate = 'next'
 
-// export default useStatusClass
+    if(statusSlider === 'next' &&  currentIndex === active ){
+        nextAnimate = 'next-next'
+    }
+    else if (statusSlider === 'next' && currentIndex > 0 && currentIndex - active === 1){
+        prevAnimate = 'next-previous'
+    }
+    else if(statusSlider === 'previous' && currentIndex === active ){
+        nextAnimate = 'previous-next'
+    }
+    else if (statusSlider === 'previous' && currentIndex > 0 && currentIndex - active === 1 ){
+        prevAnimate = 'previous-previous'
+    }
+    
+
+    return [prevAnimate , nextAnimate]
+}
+
+export default useStatusClass
